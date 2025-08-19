44f3c278cdaa0107ba543f949b218a69310474b2
nirina3 <1nirinanirina3@gmail.com>
2025-08-19 10:02:01 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       onwarn(warning, warn) {
         // Ignorer les avertissements TypeScript
@@ -17,5 +20,15 @@ export default defineConfig({
   esbuild: {
     // Ignorer les erreurs TypeScript pendant le build
     logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
+  preview: {
+    port: 4173,
+    strictPort: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://localhost:5173"
   }
 })
